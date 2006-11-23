75657ac6bfff7a435ebda1dde8aa842a343cf458
akira_ag <akira_ag@6e58b0a2-a920-0410-943a-aae568831f16>
2006-11-23 19:54:24 +0000

    Small update which able to execute script from another directory (only for WinNT)

--- 1.2/Dist/bin/bncompiler.cmd
+++ 1.2/Dist/bin/bncompiler.cmd
@@ -1,3 +1,3 @@
 @echo off
-java -classpath "../depends/lineargs.jar;../depends/antlr.jar;../depends/activation.jar;../depends/jaxb-api.jar;../depends/jaxb-impl.jar;../depends/jaxb1-impl.jar;../depends/jsr173_1.0_api.jar;../lib/java/binarynotes.jar;../bin/bncompiler.jar" org.bn.compiler.Main %1 %2 %3 %4 %5 %6 %7 %8
-
+set BN_HOME=%~dp0
+java -classpath "%BN_HOME%/../depends/lineargs.jar;%BN_HOME%/../depends/antlr.jar;%BN_HOME%/../depends/activation.jar;%BN_HOME%/../depends/jaxb-api.jar;%BN_HOME%/../depends/jaxb-impl.jar;%BN_HOME%/../depends/jaxb1-impl.jar;%BN_HOME%/../depends/jsr173_1.0_api.jar;%BN_HOME%/../lib/java/binarynotes.jar;%BN_HOME%/../bin/bncompiler.jar" org.bn.compiler.Main %1 %2 %3 %4 %5 %6 %7 %8
